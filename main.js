ecf24239399ad9490fd0c7279c6b45495bfb9ba2
Linhárt Ervin <LinhartE@olajterv.hu>
2019-09-26 11:04:18 +0200

    add basic menu  (on the video for the 16:56)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -12,8 +12,38 @@ app.on('ready', function() {
     mainWindow = new BrowserWindow({});
     //load html file to the main window
     mainWindow.loadURL(url.format({
-        pathname:path.join(__dirname,'mainWindow.html'),
+        pathname: path.join(__dirname,'mainWindow.html'),
         protocol: 'file',
         slashes: true
     }));
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    //insert menu from template
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add item',
+            },
+            {
+                label: 'Clear items',
+            },
+            {type: 'separator'},
+            {
+                label: 'Quit',
+                //add hotkey to quit
+                accelerator: process.platform == 'darwin'? 'Command+Q' : 'Control+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
